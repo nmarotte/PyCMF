26917ba803e1cc4a7603774c144480a779b14c5b
nmarotte <nathan.marotte@ulb.be>
2021-04-25 12:58:40 +0200

    Refactored out WaterComponent.py

--- Agregations/WaterComponent.py
+++ Agregations/WaterComponent.py
@@ -6,6 +6,10 @@ from Utils import neighbors
 
 
 class WaterComponent(Ticked):
+    """
+    This class could subclass np.ndarray but since it doesn't __init__, it is very complicated to make it work alongside
+    Ticked
+    """
     volume_each = 1000
     mass_each = 1000
     energy_each_start = 1255200000
@@ -16,13 +20,11 @@ class WaterComponent(Ticked):
         flat_shape = np.product(shape)
         self.components = np.empty(flat_shape, dtype=CubeOfWater)
 
-        volumes = np.full(flat_shape, fill_value=WaterComponent.volume_each)
-        masses = np.full(flat_shape, fill_value=WaterComponent.mass_each)
         energies = np.random.normal(WaterComponent.energy_each_start, 104600000, flat_shape)
         co2_ppmvs = np.random.normal(WaterComponent.co2_ppmv_each_start, 25, flat_shape)
 
         for i in range(len(self.components)):
-            self.components[i] = CubeOfWater(i, volumes[i], masses[i], energies[i], co2_ppmvs[i])
+            self.components[i] = CubeOfWater(i, WaterComponent.volume_each, WaterComponent.mass_each, energies[i], co2_ppmvs[i])
             for j in neighbors(i, shape):
                 self.components[j].add_neighbor(self.components[i])
                 self.components[i].add_neighbor(self.components[j])
@@ -36,3 +38,6 @@ class WaterComponent(Ticked):
     @property
     def flat(self):
         return self.components.flat
+
+    def __iter__(self):
+        return self.components.__iter__()
