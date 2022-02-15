2f7e69a6d0ce5e44b370c56f307dd1333c6f5dc4
nmarotte <nathan.marotte@ulb.be>
2022-02-15 13:03:14 +0100

    Removed ticking_universe.py

--- src/modelsv2/ticking_class/ticking_universe.py
+++
@@ -1,6 +0,0 @@
-from modelsv2.ABC.ticking_model import TickingModel
-from modelsv2.physical_class.universe import Universe
-
-
-class TickingUniverse(Universe, TickingModel):
-    pass
--- src/test/ticking_class/test_ticking_sun.py
+++ src/test/ticking_class/test_ticking_sun.py
@@ -1,20 +1,20 @@
 import math
 import unittest
 
+from modelsv2.physical_class.universe import Universe
 from modelsv2.ticking_class.ticking_earth import TickingEarth
 from modelsv2.ticking_class.ticking_grid_chunk import TickingGridChunk
 from modelsv2.ticking_class.ticking_sun import TickingSun
-from modelsv2.ticking_class.ticking_universe import TickingUniverse
 
 
 class TestTickingUniverse(unittest.TestCase):
-    @unittest.skipIf(TickingSun.radiate_energy_outwards not in TickingUniverse.on_tick_methods,"Not testing disabled on_tick method")
+    @unittest.skipIf(TickingSun.radiate_energy_outwards not in Universe.on_tick_methods,"Not testing disabled on_tick method")
     def test_sun_radiate_earth(self):
         earth = TickingEarth(shape=(10, 1))
         earth[0] = TickingGridChunk.from_components_tuple((1000, 0, "WATER"), volume=1, index=0, parent=earth)
         sun = TickingSun()
         # Change the energy radiated per second to match a radiation of exactly 1 Joule per time delta towards the earth
-        sun.energy_radiated_per_second = (4 * math.pi) / (sun.solid_angle(earth) * TickingUniverse.TIME_DELTA)
+        sun.energy_radiated_per_second = (4 * math.pi) / (sun.solid_angle(earth) * Universe.TIME_DELTA)
         sun.get_universe().discover_everything()
         before_energy = earth.compute_total_energy()
         sun.update()
